08b15714f8c4a5bc12210b9b4e986480f91997b2
sevansevanich <sevansevanich@mail.ru>
2015-12-11 13:46:55 +0300

    Поправил баг с многократной ионизацией

--- Ion/Ion/Run.cpp
+++ Ion/Ion/Run.cpp
@@ -59,12 +59,21 @@ struct
 {
 	double charge = 0;
 	double charge_step = el;
-	int number_of_charge_states = 6;
-	int ion_l[6] = { 1,1,0,0,0,0 };
-	int ion_n[6] = { 2,2,2,2,1,1 };
-	int ion_num_of_el[6] = { 2,1,2,1,2,1 };
-	double ion_pot[6] = { 11.26,24.38,47.88,64.49,392.09,489.99 };
+	int number_of_charge_states = 26;
+	int ion_l[26] = { 0,0,2,2,2,2,2,2,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,0,0,0};
+	int ion_n[26] = { 4, 4, 3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,1,1};
+	double ion_pot[26] = { 7.9024678, 16.19920, 30.651, 54.91, 75.0, 98.985, 124.98, 151.060, 233.6, 262.10, 290.9, 330.8, 361.0, 392.2, 456.2, 489.312, 1262.7, 1357.8, 1460, 1575.6, 1687.0, 1798.4, 1950.4, 2045.759, 8828.1875, 9277.6814};
 } data_fe;
+
+struct
+{
+	double charge = 0;
+	double charge_step = el;
+	int number_of_charge_states = 13;
+	int ion_l[13] = { 1,0,0,1,1,1,1,1,1,0,0,0,0 };
+	int ion_n[13] = { 3,2,2,2,2,2,2,2,2,2,2,1,1 };
+	double ion_pot[13] = { 5.985768, 18.82855, 28.44764, 119.9924, 153.825, 190.49, 241.76, 284.64, 330.21, 398.65, 442.005, 2085.97963, 2304.14 };
+} data_al;
 
 struct data_in
 {
@@ -83,19 +92,19 @@ int main()
 {
 	data_in data;
 
-	int z0=0, z1=0;
-	
-	z0 = data_h.charge / data_h.charge_step; // 
-	z1 = z0 + data_h.number_of_charge_states - 1; //
+	int z0 = 0, z1 = 0;
 
-	double *A = new double[z1]; 
-	double *B = new double[z1]; 
+	z0 = data_n.charge / data_n.charge_step; // 
+	z1 = z0 + data_n.number_of_charge_states - 1; //
+
+	double *A = new double[z1];
+	double *B = new double[z1];
 	double *C = new double[z1];
 
 	double W[10] = {};
-	int choose = 0, choose2=0;
-	double fil = 0, value =0, a0=0;
-	
+	int choose = 0, choose2 = 0;
+	double fil = 0, value = 0, a0 = 0;
+
 	cout << "Choose field's data:" << endl;
 	cout << "1 - E" << endl;
 	cout << "2 - I" << endl;
@@ -105,21 +114,21 @@ int main()
 	cin >> choose2;
 	cout << "Write value: ";
 	cin >> value;
-	value = 9.23*pow(10,13);
+	//value = 9.23*pow(10, 13);
 
 	switch (choose)
 	{
-	case 1: 
+	case 1:
 		fil = value;
 		break;
-	case 2: 
+	case 2:
 		if (choose2 == 1) {
 			a0 = 8.6*pow(10, -10)*0.8*sqrt(value);// / sqrt(2); //intensy in W/cm^2 for circular polirazat
 			fil = E(a0); //v/m
 		}
 		else
 		{
-			a0 = 8.6*pow(10, -10)*0.8*sqrt(value)/ sqrt(2); //intensy in W/cm^2 for circular polirazat
+			a0 = 8.6*pow(10, -10)*0.8*sqrt(value) / sqrt(2); //intensy in W/cm^2 for circular polirazat
 			fil = E(a0); //v/m
 		}
 		break;
@@ -131,33 +140,32 @@ int main()
 	for (int i = z0 + 1;i <= z1;i++)// calculation coefficients
 	{
 		int t = i - 1;
-		C[t] = C_res(data_h.ion_l[t], n(i, k(data_h.ion_pot[t])));
-		B[t] = B_res(data_h.ion_pot[t]);
-		A[t] = w_a*pow(k(data_h.ion_pot[t]), 2)*(2 * data_h.ion_l[t] + 1)*pow(B[t] * 2, (2 * n(i, k(data_h.ion_pot[t])) - 1))*C[t] / 2;
+		C[t] = C_res(data_n.ion_l[t], n(i, k(data_n.ion_pot[t])));
+		B[t] = B_res(data_n.ion_pot[t]);
+		A[t] = w_a*pow(k(data_n.ion_pot[t]), 2)*(2 * data_n.ion_l[t] + 1)*pow(B[t] * 2, (2 * n(i, k(data_n.ion_pot[t])) - 1))*C[t] / 2;
 		W[t] = A[t] * pow(E(2), -(2 * n(i, k(data_n.ion_pot[t])) - 1))*exp(-2 * B[t] / (3 * E(2)));
 	};
 
-	/*ofstream fout;
+	ofstream fout;
 	fout.open("Ion_out_ion.txt");
 
-	double a = 0, W_E_a[30] = {};
+	double a = 0.1, W_E_a[30] = {};
 	for (int i = 0; i < 30; i++)
 	{
-		W_E_a[i] = A[5] * pow(E(a), -(2 * n(6, k(data_h.ion_pot[5])) - 1))*exp(-2 * B[5] / (3 * E(a)));
-		fout << W_E_a[i] << " " << a << endl;
+		W_E_a[i] = A[5] * pow(E(a), -(2 * n(6, k(data_n.ion_pot[5])) - 1))*exp(-2 * B[5] / (3 * E(a)));
+		fout << W_E_a[i] << " " << a << " " << 1 - exp(-W_E_a[i] * 0.2668*pow(10, -14)) << endl;
 		a += 0.1;
 	}
-
-	fout.close();*/
+	fout.close();
 	
 	Particle atom[kol][kol]; // target
 
 	for (int i = 0;i < kol;i++) //target initialization for first ionization, n for the first electron
 		for (int j = 0;j < kol;j++)
 		{
-			atom[i][j].charge = data_h.charge;
+			atom[i][j].charge = data_n.charge;
 			atom[i][j].z1 = z1;
-			atom[i][j].n = n(1, k(data_h.ion_pot[0]));
+			atom[i][j].n = n(1, k(data_n.ion_pot[0]));
 			atom[i][j].dt = 0.2668*pow(10, -14);
 		};
 	
@@ -168,13 +176,13 @@ int main()
 			{
 				for (int q = atom[i][j].z_i; q < z1;q++) // calculate ionization rate for z_i<i<z1. DON'T FORGET give current n
 				{
-					atom[i][j].W_cal(A[q], B[q], fil, n(q+1, k(data_h.ion_pot[q]))); // for direct current; give function n z, which equal q+1, but this equation does't >z1!!!!!!
-					atom[i][j].W_cal_AC(A[q], B[q], fil, n(q + 1, k(data_h.ion_pot[q]))); //for alternating current 
+					atom[i][j].W_cal(A[q], B[q], fil, n(q+1, k(data_n.ion_pot[q]))); // for direct current; give function n z, which equal q+1, but this equation does't >z1!!!!!!
+					atom[i][j].W_cal_AC(A[q], B[q], fil, n(q + 1, k(data_n.ion_pot[q]))); //for alternating current 
 				}
 				atom[i][j].P();
 				if (atom[i][j].j != 0)
 				{
-					atom[i][j].charge += atom[i][j].j*data_h.charge_step; //Atom's charge will change if degree ionization doesn't equal zero (j!=0)
+					atom[i][j].charge += atom[i][j].j*data_n.charge_step; //Atom's charge will change if degree ionization doesn't equal zero (j!=0)
 					atom[i][j].j = 0;
 				};
 			}
